d56b14fea7d27d45082fc1b0493f1cbb9042daa4
juliettegodyere <juliettejuli@yahoo.com>
2025-05-24 07:33:16 +0100

    Implemented keyboard from covering input box in the footer

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import {
-  View,
   FlatList,
   StyleSheet,
   Pressable,
-  TouchableOpacity,
-  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import ShoppingListItemPage from "../../components/ShoppingListItem";
 import { Box } from "@/components/ui/box";
@@ -42,7 +43,7 @@ export default function Index() {
   const [selectedCat, setSelectedCat] = useState("All");
   const [isCreateButtonPressed, setCreateButtonPressed] = useState(false);
   const [customItem, setCustomItem] = useState("");
-  const {addUserDefinedItem } = useShoppingActions();
+  const { addUserDefinedItem } = useShoppingActions();
 
   const fetchItems = async () => {
     const data = await getAllShoppingItems(db);
@@ -104,151 +105,178 @@ export default function Index() {
   };
 
   const handleAddCustomItem = async (itemLabel: string, category: string) => {
-   
-    if (!itemLabel.trim()) return; 
+    if (!itemLabel.trim()) return;
     await addUserDefinedItem(itemLabel, category);
     fetchItems();
 
-    setCustomItem(""); 
+    setCustomItem("");
   };
 
   return (
-    <Box
+    <KeyboardAvoidingView
       style={{
         flex: 1,
         position: "relative",
       }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
     >
-      {/* Background Image */}
-      <Box
-        style={{
-          ...StyleSheet.absoluteFillObject, // fills the parent
-          zIndex: 0,
-        }}
-      >
-        <Image
-          size="full"
-          // source={{
-          //   uri: "https://gluestack.github.io/public-blog-video-assets/mountains.png",
-          // }}
-          source={require("../../assets/images/background.png")}
-          alt="background"
-          style={{ flex: 1 }}
-        />
-      </Box>
-
-      {/* Foreground Content */}
-      <Box
-        style={{
-          flex: 1,
-          zIndex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Box
-          style={{
-            height: "85%",
-            width: "85%",
-            //padding: 16,
-            position: "relative",
-            marginTop: -80,
-            backgroundColor: "#F1F1F1",
-          }}
-          className="rounded-xl shadow-lg overflow-hidden"
-        >
-          {/* Header */}
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Box style={{ flex: 1, position: "relative" }}>
+          {/* Background Image */}
           <Box
             style={{
-              height: "7%",
-              width: "100%",
-              position: "absolute",
-              top: 0,
-              left: 0,
-              zIndex: 10,
-              backgroundColor: "#F1F1F1",
+              ...StyleSheet.absoluteFillObject, // fills the parent
+              zIndex: 0,
             }}
-            className="p-3"
           >
-            <HStack space="md" className="justify-between">
-              <Heading className="text-gray-900 font-normal">
-                Shopping list
-              </Heading>
-              <Icon as={ThreeDotsIcon} className="text-gray-900" />
-            </HStack>
+            <Image
+              size="full"
+              // source={{
+              //   uri: "https://gluestack.github.io/public-blog-video-assets/mountains.png",
+              // }}
+              source={require("../../assets/images/background.png")}
+              alt="background"
+              style={{ flex: 1 }}
+            />
           </Box>
 
-          {/* Footer */}
+          {/* Foreground Content */}
           <Box
             style={{
-              height: isCreateButtonPressed ? "auto" : "7%",
-              width: "100%",
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              zIndex: 10,
-              backgroundColor: "#F1F1F1",
+              flex: 1,
+              zIndex: 1,
+              alignItems: "center",
+              justifyContent: "center",
             }}
-            className="p-3"
           >
-            {!isCreateButtonPressed ? (
-              <HStack space="md" className="justify-between items-center">
-                <HStack space="sm">
-                  <Icon as={AddIcon} className="text-typography-800" />
-                  <Pressable onPress={onPressCreateItemFunction}>
-                    <Text >Add Item</Text>
-                  </Pressable>
+            <Box
+              style={{
+                height: "85%",
+                width: "85%",
+                //padding: 16,
+                position: "relative",
+                marginTop: -80,
+                backgroundColor: "#F1F1F1",
+              }}
+              className="rounded-xl shadow-lg overflow-hidden"
+            >
+              {/* Header */}
+              <Box
+                style={{
+                  height: "7%",
+                  width: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  zIndex: 10,
+                  backgroundColor: "#F1F1F1",
+                }}
+                className="p-3"
+              >
+                <HStack space="md" className="justify-between">
+                  <Heading className="text-gray-900 font-normal">
+                    Shopping list
+                  </Heading>
+                  <Icon as={ThreeDotsIcon} className="text-gray-900" />
                 </HStack>
-                <BookImage size={15} color="#888" />
-              </HStack>
-            ) : (
-              <VStack>
-                <Input size="md" variant="outline" className=" bg-white">
-                  <InputField
-                    placeholder="Enter New Item..."
-                    value={customItem}
-                    onChangeText={setCustomItem}
-                    className="bg-white rounded-md"
+              </Box>
+
+              {/* Footer */}
+              <Box
+                style={{
+                  height: isCreateButtonPressed ? "auto" : "7%",
+                  width: "100%",
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  zIndex: 10,
+                  backgroundColor: "#F1F1F1",
+                }}
+                className="p-3"
+              >
+                {!isCreateButtonPressed ? (
+                  <HStack space="md" className="justify-between items-center">
+                    <HStack space="sm">
+                      <Icon as={AddIcon} className="text-typography-800" />
+                      <Pressable onPress={onPressCreateItemFunction}>
+                        <Text>Add Item</Text>
+                      </Pressable>
+                    </HStack>
+                    <BookImage size={15} color="#888" />
+                  </HStack>
+                ) : (
+                  <VStack>
+                    <Input size="md" variant="outline" className=" bg-white">
+                      <InputField
+                        placeholder="Enter New Item..."
+                        value={customItem}
+                        onChangeText={setCustomItem}
+                        className="bg-white rounded-md"
+                      />
+                      <InputSlot className="pr-3 bg-white">
+                        <AntDesignIcon
+                          size={15}
+                          name="arrowsalt"
+                          color="#888"
+                          onPress={() => setShowModal(true)}
+                        />
+                      </InputSlot>
+                    </Input>
+                    <HStack
+                      space="md"
+                      className="justify-between items-center pt-2"
+                    >
+                      <Pressable onPress={onPressCancelFunction}>
+                        <Text
+                          className=" font-medium"
+                          style={{ color: "#FF6347" }}
+                        >
+                          Cancel
+                        </Text>
+                      </Pressable>
+                      <Pressable
+                        onPress={() =>
+                          handleAddCustomItem(customItem, "uncategorized")
+                        }
+                      >
+                        <Text
+                          className="font-extrabold"
+                          style={{ color: "#FF6347" }}
+                        >
+                          Add
+                        </Text>
+                      </Pressable>
+                    </HStack>
+                  </VStack>
+                )}
+              </Box>
+
+              {/* Scrollable Content */}
+              <FlatList
+                contentContainerStyle={{
+                  padding: 5,
+                  // paddingTop: "10%",
+                  // paddingBottom: "10%",
+                  paddingTop: isCreateButtonPressed ? "16%" : "10%",
+                  paddingBottom: isCreateButtonPressed ? "16%" : "10%",
+                }}
+                data={inventoryItems}
+                renderItem={({ item }) => (
+                  <ShoppingListItemPage
+                    shoppingList={item}
+                    isModalOpen={showModal}
+                    onCloseModal={() => setShowModal(false)}
                   />
-                  <InputSlot className="pr-3 bg-white" >
-                    <AntDesignIcon size={15} name="arrowsalt" color="#888" onPress={() => setShowModal(true)}/>
-                  </InputSlot>
-                </Input>
-                <HStack space="md" className="justify-between items-center pt-2">
-                  <Pressable onPress={onPressCancelFunction}>
-                    <Text className=" font-medium" style={{ color: "#FF6347" }}>
-                      Cancel
-                    </Text>
-                  </Pressable>
-                  <Pressable onPress={() => handleAddCustomItem(customItem, "uncategorized")}>
-                    <Text className="font-extrabold" style={{ color: "#FF6347" }}>
-                      Add
-                    </Text>
-                  </Pressable>
-                </HStack>
-              </VStack>
-            )}
+                )}
+                keyExtractor={(item) => item.value}
+                showsVerticalScrollIndicator={false}
+              />
+            </Box>
           </Box>
-
-          {/* Scrollable Content */}
-          <FlatList
-            contentContainerStyle={{
-              padding: 5,
-              // paddingTop: "10%",
-              // paddingBottom: "10%",
-              paddingTop: isCreateButtonPressed ? "16%" : "10%",
-              paddingBottom: isCreateButtonPressed ? "16%" : "10%",
-            }}
-            data={inventoryItems}
-            renderItem={({ item }) => (
-              <ShoppingListItemPage shoppingList={item} isModalOpen={showModal} onCloseModal={() => setShowModal(false)}/>
-            )}
-            keyExtractor={(item) => item.value}
-            showsVerticalScrollIndicator={false}
-          />
         </Box>
-      </Box>
-    </Box>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
